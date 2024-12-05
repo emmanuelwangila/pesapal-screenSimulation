9cddb426d5f8ab33868d6231bca84b940e80c838
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-12-06 00:13:28 +0300

    refactors

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import curses
 def simpleScreenSimulation(screen):
     curses.curs_set(0)  # Hide cursor
     screen.clear()
-    screen.addstr(0, 0, "Hello, this is a test!")
+    screen.addstr(0, 0, "Hello, ")
     screen.refresh()
     screen.getch()  # Wait for user input before exiting
 
